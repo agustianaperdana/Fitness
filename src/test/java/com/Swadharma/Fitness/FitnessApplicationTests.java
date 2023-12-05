65505a90004348ab3cac72009279fcb84d87960d
perdana <perdanashop2019@gmail.com>
2023-12-05 00:21:04 -0800

    test

---
+++ src/test/java/com/Swadharma/Fitness/FitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Swadharma.Fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
